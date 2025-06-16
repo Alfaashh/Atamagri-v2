2ea342dc0b2c88f6cd8a1be31b1304972df30118
Al Fathi Ayyash <150584442+Alfaashh@users.noreply.github.com>
2025-06-16 23:23:07 +0700

    Create page.tsx

---
+++ app/firebase-test/page.tsx
@@ -0,0 +1,6 @@
+// app/firebase-test/page.tsx
+import FirebaseTestDashboard from '@/components/FirebaseTestDashboard';
+
+export default function FirebaseTestPage() {
+  return <FirebaseTestDashboard />;
+}
