41d1eaea37a715e43bea36ad62d059834ed734f6
Al Fathi Ayyash <150584442+Alfaashh@users.noreply.github.com>
2025-06-19 10:19:16 +0700

    Update timeAgo.ts

--- lib/timeAgo.ts
+++ lib/timeAgo.ts
@@ -1,6 +1,7 @@
 import { formatDistanceToNow } from "date-fns";
+import { id } from "date-fns/locale";
 
 export function timeAgo(date: Date | number | string): string {
   const d = typeof date === "string" || typeof date === "number" ? new Date(date) : date;
-  return formatDistanceToNow(d, { addSuffix: true });
+  return formatDistanceToNow(d, { addSuffix: true, locale: id });
 }
